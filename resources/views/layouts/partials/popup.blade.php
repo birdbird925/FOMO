ef794f529e3616d4c3bba258bc3f3c5794d00877
xiangwen94 <xiangwen94@gmail.com>
2017-04-26 14:44:53 +0800

    message popup css 2

--- resources/views/layouts/partials/popup.blade.php
+++ resources/views/layouts/partials/popup.blade.php
@@ -1,15 +1,17 @@
-<div class="msg-popup {{session('popup')  ? 'popup' : ''}} ">
+<div class="msg-popup {{session('popup')  ? 'popup' : ''}} popup">
     <div class="close-nav {{session('popup')  ? 'popup' : ''}}"></div>
     <div class="popup-inner">
         <div class="title">
             @if(session('popup'))
                 {{session('popup.title')}}
             @endif
+            Uh - oh!
         </div>
         <div class="caption">
             @if(session('popup'))
                 {{session('popup.caption')}}
             @endif
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad tempore nam facilis, laboriosam totam. 
         </div>
     </div>
 </div>
